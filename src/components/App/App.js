98a008d5adbd15c1f1d933e44e8f609cd98da5ac
Helen Shin <helenhyshin@gmail.com>
2022-12-09 14:09:39 -0800

    initial commit

---
+++ src/components/App/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Navbar from "../Navbar/Navbar";
+import Title from "../Title/Title";
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Title />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Title/Title.js
@@ -0,0 +1,11 @@
+import "./Title.css";
+
+function Title() {
+  return (
+    <div className="title">
+      <h1>Hello from Title</h1>
+    </div>
+  );
+}
+
+export default Title;
